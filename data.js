1ae7c51df15b113f651be1b441a87479865d038b
ADITYA REDDY SAMA <adityareddy590@gmail.com>
2025-11-30 15:43:35 +0530

    Delete data.js

--- data.js
+++
@@ -1,7 +0,0 @@
-import mongoose from 'mongoose';
-export const entry=new mongoose.Schema({
-    name:String,
-    email:{type:String,unique:true},
-    password:String
-});
-// export const user=mongoose.model("second",entry);
