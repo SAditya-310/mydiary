5595c46a3be2d088fa5a0d9fee82cd4f7fc4ec55
ADITYA REDDY SAMA <adityareddy590@gmail.com>
2025-11-30 15:45:04 +0530

    Delete notemodel.js

--- notemodel.js
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-export const note=new mongoose.Schema({
-    userId: { type: mongoose.Schema.Types.ObjectId, ref: 'second', required: true },
-    title:String,
-    content:String,
-    mood:{ type: String,default:"neutral" },
-    date:{type:String}
-});
-// export const notes=mongoose.model("notes",note);
